b13b8a2c77af5d032c9a2e1d8abd37a9ae2656bd
Mangrove-Sun <tjsghd0317@gmail.com>
2022-07-14 23:02:54 +0900

    배포 재확인

--- api/test.ts
+++ api/test.ts
@@ -2,6 +2,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({
-    name: 'Sun!'
+    name: 'Sun!!'
   })
 }
